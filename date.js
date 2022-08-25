81d14646384a73f6217c5494e377bb8dd1dab403
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-25 14:33:12 +0200

    3 done

--- date.js
+++ date.js
@@ -3,19 +3,17 @@ console.log("Aufgabe2");
 const now = new Date();
 const test = new Date();
 //1. Was ist die Variable `now` in dem obigen Code?
-console.log(
-    "1. Die variable now ist eine abgespeicherte function die Datum Uhrzeit und Zeitzone"
-);
+console.log("1. Eine Instnze der klasse date");
 //2. Welchen Typ hat die Variable `now`?
 console.log("2. now hat den typ object");
 //3. Was ist in dem obigen Code die Variable `Date`?
 console.log("3. eine classes");
 //4. Was ist der Typ von `Date`?
-console.log("4. DateAndTime-Klasse");
+console.log("4. Function");
 //5. Was erhältst du, wenn du `console.log(now)` eingibst?
 console.log("5. output von Date function");
 //6. Was erhältst du, wenn du `console.log(Date)` eingibst?
-console.log("6. kein output da () fehlen");
+console.log("6. function date");
 //7. Was erhältst du, wenn du `console.log(new Date())` eingibst?
 console.log("7. aktuelle Date-Time");
 //8. Ist `now` wahrheitsgemäß?
