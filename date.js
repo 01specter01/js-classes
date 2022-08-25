955d74989854acf365e46a4b89a003cd626a7a60
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-25 12:04:31 +0200

    2 done

---
+++ date.js
@@ -0,0 +1,30 @@
+console.log("Aufgabe2");
+
+const now = new Date();
+const test = new Date();
+//1. Was ist die Variable `now` in dem obigen Code?
+console.log(
+    "1. Die variable now ist eine abgespeicherte function die Datum Uhrzeit und Zeitzone"
+);
+//2. Welchen Typ hat die Variable `now`?
+console.log("2. now hat den typ object");
+//3. Was ist in dem obigen Code die Variable `Date`?
+console.log("3. eine classes");
+//4. Was ist der Typ von `Date`?
+console.log("4. DateAndTime-Klasse");
+//5. Was erhältst du, wenn du `console.log(now)` eingibst?
+console.log("5. output von Date function");
+//6. Was erhältst du, wenn du `console.log(Date)` eingibst?
+console.log("6. kein output da () fehlen");
+//7. Was erhältst du, wenn du `console.log(new Date())` eingibst?
+console.log("7. aktuelle Date-Time");
+//8. Ist `now` wahrheitsgemäß?
+console.log("8. ja ");
+//9. Was bekommst du, wenn du `console.log(now === test)` eingibst? Und warum?
+console.log("9. gibt false");
+//10. Was bekommst du, wenn du `console.log(now === Date)` eingibst?
+console.log("10. gibt false");
+//11. Was bekommst du, wenn du `console.log(now === new Date())` machst?
+console.log("11. gibt false");
+//12. Was bekommst du, wenn du `console.log(new Date() === new Date())` machst? Und warum?
+console.log("12. gibt false");
